a651231b82aaa60e576cfae3d62850c5369f67f0
X1nto <burtkni@yandex.ru>
2022-01-14 03:15:55 +0400

    add collection builder methods

---
+++ Tests/Collection/Builders.cs
@@ -0,0 +1,33 @@
+﻿namespace Tests.Collection;
+
+using System.Collections.Generic;
+using NUnit.Framework;
+
+using static Sharplin.Collection.Builders;
+
+public class Builders
+{
+    [Test]
+    public void Test_ArrayOf()
+    {
+        string[] expected = {"this", "is", "a", "test"};
+        string[] actual = ArrayOf("this", "is", "a", "test");
+        Assert.AreEqual(expected, actual);
+    }
+    
+    [Test]
+    public void Test_ListOf()
+    {
+        var expected = new List<string> {"this", "is", "a", "test"};
+        var actual = ListOf("this", "is", "a", "test");
+        Assert.AreEqual(expected, actual);
+    }
+    
+    [Test]
+    public void Test_HashSetOf()
+    {
+        var expected = new HashSet<string> {"this", "is", "a", "test"};
+        var actual = HashSetOf("this", "is", "a", "test");
+        Assert.AreEqual(expected, actual);
+    }
+}
