7cd6ac043c697538b97e4c60af05f9938330bab0
X1nto <burtkni@yandex.ru>
2022-01-13 16:47:41 +0400

    Add tests for margin and indent

---
+++ Tests/String/Indent.cs
@@ -0,0 +1,27 @@
+﻿namespace Tests.String;
+
+using NUnit.Framework;
+using Sharplin.String;
+
+public class Indent
+{
+    [Test]
+    public void Test_TrimIndent()
+    {
+        Assert.AreEqual("123", @"
+        123
+        ".TrimIndent());
+        Assert.AreEqual("123\n    456", @"
+        123
+            456
+        ".TrimIndent());
+        Assert.AreEqual("    123\n456", @"
+            123
+        456
+        ".TrimIndent());
+        Assert.AreEqual("|    123\n|456", @"
+            123
+        456
+        ".ReplaceIndent(newIndent: "|"));
+    }
+}
---
+++ Tests/String/Margin.cs
@@ -0,0 +1,22 @@
+﻿namespace Tests.String;
+
+using NUnit.Framework;
+using Sharplin.String;
+
+public class Margin
+{
+    [Test]
+    public void Test_TrimMargin()
+    {
+        Assert.AreEqual("ABC\n123\n456", @"
+            |ABC
+            |123
+            |456
+        ".TrimMargin());
+        Assert.AreEqual("ABC\n123\n456", @"
+            #ABC
+            #123
+            #456
+        ".TrimMargin(marginPrefix: "#"));
+    }
+}
