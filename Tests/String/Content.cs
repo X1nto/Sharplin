01c389a0a072ac317fbd60cc9816d8e68f3acbf8
X1nto <burtkni@yandex.ru>
2022-01-13 16:10:31 +0400

    add tests for IsEmpty and IsBlank

--- Tests/String/Content.cs
+++ Tests/String/Content.cs
@@ -14,13 +14,25 @@ public class Content
     [Test]
     public void Test_IsBlank()
     {
+        void TestIsBlank(string actual)
+        {
+            Assert.IsTrue(actual.IsBlank());
+        }
         
+        TestIsBlank(" ");
+        TestIsBlank("");
     }
 
     [Test]
     public void Test_IsEmpty()
     {
+        void TestIsEmpty(string actual)
+        {
+            Assert.IsTrue(actual.IsEmpty());
+        }
         
+        TestIsEmpty(" ");
+        TestIsEmpty("");
     }
     
     [Test]
