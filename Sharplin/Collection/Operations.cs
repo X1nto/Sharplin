a1f0692297d34ca97e770ade618fa3bbf5c7d590
X1nto <burtkni@yandex.ru>
2022-01-13 15:05:37 +0400

    add foreach operations

---
+++ Sharplin/Collection/Operations.cs
@@ -0,0 +1,20 @@
+﻿namespace Sharplin.Collection;
+
+public static class Loops
+{
+    public static void ForEach<TSource>(this IEnumerable<TSource> source, Action<TSource> action)
+    {
+        foreach (TSource item in source)
+        {
+            action(item);
+        }
+    }
+
+    public static void ForEachIndexed<TSource>(this IList<TSource> source, Action<int, TSource> action)
+    {
+        foreach (int index in source.EIndices())
+        {
+            action(index, source[index]);
+        }
+    }
+}
