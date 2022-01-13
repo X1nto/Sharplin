8798c0b3e21ca5051c2376bcc46d5afd38c04212
X1nto <burtkni@yandex.ru>
2022-01-13 16:05:08 +0400

    add unit tests

---
+++ Tests/String/Content.cs
@@ -0,0 +1,53 @@
+﻿namespace Tests.String;
+
+using NUnit.Framework;
+using Sharplin.String;
+
+public class Content
+{
+    [SetUp]
+    public void SetUp()
+    {
+        
+    }
+
+    [Test]
+    public void Test_IsBlank()
+    {
+        
+    }
+
+    [Test]
+    public void Test_IsEmpty()
+    {
+        
+    }
+    
+    [Test]
+    public void Test_Capitalization()
+    {
+        void TestCapitalize(string expected, string actual)
+        {
+            Assert.AreEqual(expected, actual.Capitalize());
+        }
+        TestCapitalize("A", "A");
+        TestCapitalize("A", "a");
+        TestCapitalize("Abcd", "Abcd");
+        TestCapitalize("Abcd", "abcd");
+    }
+    
+    [Test]
+    public void Test_Decapitalization()
+    {
+        void TestDecapitalize(string expected, string actual)
+        {
+            Assert.AreEqual(expected, actual.Decapitalize());
+        }
+        TestDecapitalize("a", "A");
+        TestDecapitalize("a", "a");
+        TestDecapitalize("abcd", "Abcd");
+        TestDecapitalize("abcd", "abcd");
+    }
+    
+    
+}
