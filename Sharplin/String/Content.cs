3652e2c10dbb5a55b7d8b96507812aaaa8322516
X1nto <burtkni@yandex.ru>
2022-01-13 15:29:27 +0400

    add capitalize and decapitalize

--- Sharplin/String/Content.cs
+++ Sharplin/String/Content.cs
@@ -1,8 +1,18 @@
 ﻿namespace Sharplin.String;
 
+using System.Globalization;
+
 public static class Content
 {
     public static bool IsBlank(this string source) => string.IsNullOrEmpty(source) || string.IsNullOrWhiteSpace(source);
 
     public static bool IsEmpty(this string source) => string.IsNullOrWhiteSpace(source);
+
+    public static string Capitalize(this string source) => char.ToUpperInvariant(source[0]) + source.Remove(0);
+
+    public static string Capitalize(this string source, CultureInfo cultureInfo) => char.ToUpper(source[0], cultureInfo) + source.Remove(0);
+    
+    public static string Decapitalize(this string source) => char.ToLower(source[0]) + source.Remove(0);
+    
+    public static string Deapitalize(this string source, CultureInfo cultureInfo) => char.ToLower(source[0], cultureInfo) + source.Remove(0);
 }
