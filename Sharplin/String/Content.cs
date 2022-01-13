699e5dd881d0ed0b001fbd3abd1a1e91144ec946
X1nto <burtkni@yandex.ru>
2022-01-13 15:17:33 +0400

    add string content operations

---
+++ Sharplin/String/Content.cs
@@ -0,0 +1,8 @@
+﻿namespace Sharplin.String;
+
+public static class Content
+{
+    public static bool IsBlank(this string source) => string.IsNullOrEmpty(source) || string.IsNullOrWhiteSpace(source);
+
+    public static bool IsEmpty(this string source) => string.IsNullOrWhiteSpace(source);
+}
