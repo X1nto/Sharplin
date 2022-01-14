5fa8fe0f45dfe1e73a7976da69122650424219c9
X1nto <burtkni@yandex.ru>
2022-01-14 17:03:55 +0400

    add missing docs for index methods

--- Sharplin/Collection/Indexes.cs
+++ Sharplin/Collection/Indexes.cs
@@ -1,5 +1,7 @@
 ﻿namespace Sharplin.Collection;
 
+using System.Collections.Generic;
+
 public static class Indexes
 {
     /// <returns>
@@ -86,10 +88,13 @@ public static class Indexes
         return -1;
     }
 
+    /// <returns><see cref="Range"/> of valid indices for the <paramref name="source"/>.</returns>
     public static Range Indices<TSource>(this ICollection<TSource> source) => ..source.Count;
 
+    /// <returns><see cref="Enumerable.Range(int, int)"/> for the <paramref name="source"/>.</returns>
     public static IEnumerable<int> EIndices<TSource>(this ICollection<TSource> source) =>
         Enumerable.Range(0, source.Count);
 
+    /// <returns>Index of the last element in <paramref name="source"/>.</returns>
     public static int LastIndex<TSource>(this ICollection<TSource> source) => source.Count - 1;
 }
