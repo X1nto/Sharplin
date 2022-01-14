745d7dd9be1bd82095d17d2c80dc270b5689a59a
X1nto <burtkni@yandex.ru>
2022-01-14 14:19:42 +0400

    add dictionary utils

---
+++ Sharplin/Dictionary/Builders.cs
@@ -0,0 +1,40 @@
+﻿namespace Sharplin.Dictionary;
+
+using System.Collections.Generic;
+
+/// <summary>Helper class containing methods for quickly building a dictionary.</summary>
+/// <example>
+/// <para>
+/// Example 1:
+/// <code>
+/// using static Sharplin.Dictionary.Builders;
+/// var arr = DictionaryOf(PairOf("number", 1));
+/// </code>
+/// </para>
+/// <para>
+/// Example 2:
+/// <code>
+/// using Sharplin.Dictionary;
+/// var dic = Builders.DictionaryOf(PairOf("number", 1));
+/// </code>
+/// </para>
+/// <para>
+/// Example 3:
+/// <code>
+/// //GlobalUsings.cs
+/// global static using Sharplin.Dictionary.Builders;
+/// //Any other class/file in the project
+/// var dic = DictionaryOf(PairOf("number", 1));
+/// </code>
+/// </para>
+/// </example>
+public static class Builders
+{
+    /// <returns>A dictionary containing the provided <paramref name="pairs"/></returns>
+    public static Dictionary<TKey, TValue> DictionaryOf<TKey, TValue>(params KeyValuePair<TKey, TValue>[] pairs)
+        where TKey : notnull
+     => pairs.ToDictionary(pair => pair.Key, pair => pair.Value);
+
+    /// <returns>A <see cref="KeyValuePair{TKey,TValue}"/> of the provided <paramref name="key"/> and <paramref name="value"/></returns>
+    public static KeyValuePair<TKey, TValue> PairOf<TKey, TValue>(TKey key, TValue value) => new(key, value);
+}
---
+++ Tests/Dictionary/Builders.cs
@@ -0,0 +1,19 @@
+﻿namespace Tests.Dictionary;
+
+using System.Collections.Generic;
+using NUnit.Framework;
+
+public class Builders
+{
+    [Test]
+    public void Test_DictionaryOf()
+    {
+        var expected = new Dictionary<string, int>
+        {
+            {"number", 1},
+            {"money", 50}
+        };
+        var actual = DictionaryOf(PairOf("number", 1), PairOf("money", 50));
+        Assert.AreEqual(expected, actual);
+    }
+}
