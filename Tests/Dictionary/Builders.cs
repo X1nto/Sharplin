b2b6ef4d7ff9f3953d338b883992da5e844ee90e
X1nto <burtkni@yandex.ru>
2022-01-14 15:02:38 +0400

    move expected to a separate variable

--- Tests/Dictionary/Builders.cs
+++ Tests/Dictionary/Builders.cs
@@ -6,27 +6,23 @@ using Sharplin.Dictionary;
 
 public class Builders
 {
+    private static readonly Dictionary<string, int> Expected = new()
+    {
+        {"number", 1},
+        {"money", 50},
+    };
+    
     [Test]
     public void Test_DictionaryOf_PairOf()
     {
-        var expected = new Dictionary<string, int>
-        {
-            {"number", 1},
-            {"money", 50},
-        };
         var actual = DictionaryOf(PairOf("number", 1), PairOf("money", 50));
-        Assert.AreEqual(expected, actual);
+        Assert.AreEqual(Expected, actual);
     }
     
     [Test]
     public void Test_DictionaryOf_Pair()
     {
-        var expected = new Dictionary<string, int>
-        {
-            {"number", 1},
-            {"money", 50},
-        };
         var actual = DictionaryOf("number".Pair(1), "money".Pair(50));
-        Assert.AreEqual(expected, actual);
+        Assert.AreEqual(Expected, actual);
     }
 }
