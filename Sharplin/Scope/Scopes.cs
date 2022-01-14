6a854aa022cfab4cd75c4994e539b6f7d226ef8c
X1nto <burtkni@yandex.ru>
2022-01-14 16:56:07 +0400

    add Let and Apply methods

---
+++ Sharplin/Scope/Scopes.cs
@@ -0,0 +1,21 @@
+﻿namespace Sharplin.Scope;
+
+public static class Scopes
+{
+    /// <summary>
+    /// Calls the provided <paramref name="block"/> function with <paramref name="source"/> as its argument.
+    /// </summary>
+    /// <typeparam name="TResult">Result of the <paramref name="block"/> invocation.</typeparam>
+    /// <returns><typeparamref name="TResult"/></returns>
+    public static TResult Let<TSource, TResult>(this TSource source, Func<TSource, TResult> block) => block(source);
+    
+    /// <summary>
+    /// Calls the provided <paramref name="block"/> function with <paramref name="source"/> as its argument.
+    /// </summary>
+    /// <returns><paramref name="source"/></returns>
+    public static TSource Apply<TSource>(this TSource source, Action<TSource> block)
+    {
+        block(source);
+        return source;
+    }
+}
