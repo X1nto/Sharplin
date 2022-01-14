ec3f497bdc6b5ac05004040fa1971a430aa31894
X1nto <burtkni@yandex.ru>
2022-01-14 17:29:13 +0400

    add tests for let and apply

---
+++ Tests/Collection/Scopes.cs
@@ -0,0 +1,28 @@
+﻿namespace Tests.Collection;
+
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using Sharplin.Scope;
+
+public class Scopes
+{   
+    [Test]
+    public void Test_Apply()
+    {
+        var expected = new List<string> { "this", "is", "a", "test" }; 
+        var actual = new List<string> {"this", "is", "a"};
+        Assert.AreEqual(expected, actual.Apply(list => list.Add("test")));
+    }
+
+    [Test]
+    public void Test_Let()
+    {
+        var testList = new List<string> { "this", "is", "a" };
+        Assert.AreEqual("test", testList.Let(list =>
+        {
+            list.Add("test");
+            return list.ElementAt(3);
+        }));
+    }
+}
