6421cf8a06666bc664d4ff690b0926dfd2eb43c1
X1nto <burtkni@yandex.ru>
2022-01-14 18:38:41 +0400

    remove useless methods

--- Sharplin/Collection/Operations.cs
+++ Sharplin/Collection/Operations.cs
@@ -22,18 +22,4 @@ public static class Operations
     {
         foreach (int index in source.EIndices()) action(index, source[index]);
     }
-
-    /// <summary>
-    ///     Performs the given <paramref name="action"/> on each element of the <paramref name="source"/>,
-    ///     providing sequential index with the element.
-    /// </summary>
-    /// <param name="action">
-    ///     lambda that takes the index of an element and the element itself
-    ///     and performs the action on the element.
-    /// </param>
-    [Obsolete("This method is obsolete, use ForEach instead.")]
-    public static void ForEachIndexed<TSource>(this IList<TSource> source, Action<int, TSource> action)
-    {
-        foreach (int index in source.EIndices()) action(index, source[index]);
-    }
 }
--- Tests/Collection/Index.cs
+++ Tests/Collection/Index.cs
@@ -8,36 +8,7 @@ using Sharplin.Collection;
 
 public class Index
 {
-    private static readonly IEnumerable<string> ETestArr = new TestEnumerable();
     private static readonly string[] TestArr = {"a", "b", "c", "c"};
-
-    [Test]
-    public void Test_IndexOfFirst_Enumerable()
-    {
-        Assert.AreEqual(-1, ETestArr.IndexOfFirst(s => s == "d"));
-        Assert.AreEqual(2, ETestArr.IndexOfFirst(s => s == "c"));
-    }
-
-    [Test]
-    public void Test_IndexOfFirst_List()
-    {
-        Assert.AreEqual(-1, TestArr.IndexOfFirst(s => s == "d"));
-        Assert.AreEqual(2, TestArr.IndexOfFirst(s => s == "c"));
-    }
-
-    [Test]
-    public void Test_IndexOfLast_Enumerable()
-    {
-        Assert.AreEqual(-1, ETestArr.IndexOfLast(s => s == "d"));
-        Assert.AreEqual(3, ETestArr.IndexOfLast(s => s == "c"));
-    }
-
-    [Test]
-    public void Test_IndexOfLast_List()
-    {
-        Assert.AreEqual(-1, TestArr.IndexOfLast(s => s == "d"));
-        Assert.AreEqual(3, TestArr.IndexOfLast(s => s == "c"));
-    }
 
     [Test]
     public void Test_LastIndex()
@@ -55,14 +26,5 @@ public class Index
     public void Test_EIndices()
     {
         Assert.AreEqual(Enumerable.Range(0, 4), TestArr.EIndices());
-    }
-
-    private class TestEnumerable : IEnumerable<string>
-    {
-        private readonly List<string> _testList = new() {"a", "b", "c", "c"};
-
-        public IEnumerator GetEnumerator() => _testList.GetEnumerator();
-
-        IEnumerator<string> IEnumerable<string>.GetEnumerator() => _testList.GetEnumerator();
     }
 }
